4604e3bb1a73dcf143db1ba8b1d843b50aa483a3
Soin <soin@MacBook-Pro-de-Soin.local>
2019-07-23 10:19:36 -0600

    Fix high and low to forecast 2

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find weather.', undefined)
         } else{
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' temperature. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperaturesLow)
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' temperature. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow)
         }
     })
 }
